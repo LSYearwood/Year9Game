56efd861eac085aaf7ef15b69c864f8df13ad8bf
LSYearwood <70383000+LSYearwood@users.noreply.github.com>
2022-01-17 12:51:15 -0500

    Add files via upload

---
+++ sortArray.py
@@ -0,0 +1,16 @@
+import random
+
+def sortKey(k):
+    return k[1]
+
+myList = []
+
+for i in range (0,100):
+    myList += [[random.randint(1,100), chr(random.randint(65,90))]]
+
+#myList.sort()
+myList.sort(key=sortKey)
+
+for n in myList:
+    print(n)
+
